54ed5e0fc3e187a1081367ef17700a578fa4d0fc
Vinicius Macedo <viniciusmacedo010@outlook.com>
2025-06-09 21:36:04 -0300

    AJUSTES

--- padel/database/migrations/2025_06_04_132347_create_partidas_table.php
+++ padel/database/migrations/2025_06_04_132347_create_partidas_table.php
@@ -6,19 +6,25 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::create('partidas', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('torneio_id')->constrained()->onDelete('cascade');
+            $table->foreignId('torneio_id')->constrained('torneios')->onDelete('cascade');
             $table->foreignId('jogador1_id')->constrained('jogadors')->onDelete('cascade');
             $table->foreignId('jogador2_id')->constrained('jogadors')->onDelete('cascade');
-            $table->string('resultado')->nullable(); // Exemplo: "6x3 3x6 6x4"
+            $table->string('resultado')->nullable(); // Exemplo: "6x3 6x4"
             $table->timestamp('data_partida')->nullable();
             $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('partidas');
