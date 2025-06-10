554f5c8011e45f36ced223d4040de33d5e714cb7
Vinicius Macedo <viniciusmacedo010@outlook.com>
2025-06-09 22:17:56 -0300

    COMMIT

--- padel/resources/views/jogadors/create.blade.php
+++ padel/resources/views/jogadors/create.blade.php
@@ -2,14 +2,23 @@
 
 @section('content')
 <div class="container">
-    <h1>Cadastrar Jogador</h1>
+    <h1 class="mb-4">Cadastrar Jogador</h1>
     <form action="{{ route('jogadors.store') }}" method="POST">
         @csrf
-        <div class="form-group">
-            <label for="nome">Nome do Jogador</label>
+        <div class="mb-3">
+            <label for="nome" class="form-label">Nome do Jogador</label>
             <input type="text" name="nome" class="form-control" required>
         </div>
-        <button type="submit" class="btn btn-success mt-2">Salvar</button>
+        <div class="mb-3">
+            <label for="genero" class="form-label">Gênero</label>
+            <select name="genero" class="form-select" required>
+                <option value="">Selecione</option>
+                <option value="masculino">Masculino</option>
+                <option value="feminino">Feminino</option>
+                <option value="misto">Misto</option>
+            </select>
+        </div>
+        <button type="submit" class="btn btn-success">Salvar</button>
     </form>
 </div>
 @endsection
